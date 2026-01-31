21ed62f0ae59146b7cb1b5aee45cd7d933f01966
Bogdan Chayka <118861898+bigboggy@users.noreply.github.com>
2026-01-30 18:44:25 -0800

    chore: remove surname

--- compiler/examples/public/js/UserList.js
+++ compiler/examples/public/js/UserList.js
@@ -5,7 +5,6 @@ export function UserList() {
   // State
   const $users = signal([]);
   const $name = signal("");
-  const $surname = signal("");
   const $email = signal("");
   const $loading = signal(true);
 
@@ -16,7 +15,7 @@ export function UserList() {
 
   // Render
   const render = () => {
-    return h("div", { class: "container-3cb5" }, [h("h1", null, "User Management"), h("div", { class: "form-3cb5" }, [h("input", { value: $name, placeholder: "Name", oninput: (e) => { $name.value = e.target.value; } }, null), h("input", { value: $email, placeholder: "Email", oninput: (e) => { $email.value = e.target.value; } }, null), h("input", { value: $surname, placeholder: "Surname", oninput: (e) => { $surname.value = e.target.value; } }, null), h("button", { onclick: () => { fetch('/api/users', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({"name":$name.value,"email":$email.value}) }).then(r => r.json()).then(data => { $users.value = [...$users.value, data]; $name.value = ''; $email.value = ''; }) } }, "Add User")]), $loading.value ? [h("p", null, "Loading...")] : [], list($users, (item, index) => { item.index = index; return [h("div", { class: "card-3cb5" }, [h("div", { class: "user-info-3cb5" }, [h("h3", null, item.name), h("p", null, item.email)]), h("button", { class: "delete-3cb5", onclick: () => { fetch(`/api/users/${item.id}`, { method: 'DELETE' }).then(() => { $users.value = $users.value.filter(x => x.id !== item.id); }) } }, "Delete")])]; })]);
+    return h("div", { class: "container-3cb5" }, [h("h1", null, "User Management"), h("div", { class: "form-3cb5" }, [h("input", { value: $name, placeholder: "Name", oninput: (e) => { $name.value = e.target.value; } }, null), h("input", { value: $email, placeholder: "Email", oninput: (e) => { $email.value = e.target.value; } }, null), h("button", { onclick: () => { fetch('/api/users', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({"name":$name.value,"email":$email.value}) }).then(r => r.json()).then(data => { $users.value = [...$users.value, data]; $name.value = ''; $email.value = ''; }) } }, "Add User")]), $loading.value ? [h("p", null, "Loading...")] : [], list($users, (item, index) => { item.index = index; return [h("div", { class: "card-3cb5" }, [h("div", { class: "user-info-3cb5" }, [h("h3", null, item.name), h("p", null, item.email)]), h("button", { class: "delete-3cb5", onclick: () => { fetch(`/api/users/${item.id}`, { method: 'DELETE' }).then(() => { $users.value = $users.value.filter(x => x.id !== item.id); }) } }, "Delete")])]; })]);
   };
 
   return render;
