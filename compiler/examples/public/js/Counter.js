f77daea896406d91f701694950bfcd8ca4a06b6a
Bogdan Chayka <118861898+bigboggy@users.noreply.github.com>
2026-01-30 20:13:01 -0800

    chore: add reactivity to counter

--- compiler/examples/public/js/Counter.js
+++ compiler/examples/public/js/Counter.js
@@ -7,7 +7,7 @@ export function Counter() {
 
   // Render
   const render = () => {
-    return h("div", { class: "counter-rnr4" }, [h("h1", null, ["Count: ", $count.value]), h("div", { class: "buttons-rnr4" }, [h("button", { onclick: () => { $count.value--; } }, "-"), h("button", { onclick: () => { $count.value = 0; } }, "Reset"), h("button", { onclick: () => { $count.value++; } }, "+")]), $count.value > 10 ? [h("p", { class: "warning-rnr4" }, "Getting high!")] : []]);
+    return h("div", { class: "counter-rnr4" }, [h("h1", null, ["Count:  ", $count]), h("div", { class: "buttons-rnr4" }, [h("button", { onclick: () => { $count.value--; } }, "-"), h("button", { onclick: () => { $count.value = 0; } }, "Reset"), h("button", { onclick: () => { $count.value++; } }, "+")]), $count.value > 10 ? [h("p", { class: "warning-rnr4" }, "Getting high!")] : []]);
   };
 
   return render;
